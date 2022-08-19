d90de21f884c13f41087cfa1147ed1c917af1424
José Valim <jose.valim@dashbit.co>
2022-08-19 17:15:35 +0200

    Release v1.2.3

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Plug.Crypto.MixProject do
   use Mix.Project
 
-  @version "1.2.2"
+  @version "1.2.3"
   @description "Crypto-related functionality for the web"
   @source_url "https://github.com/elixir-plug/plug_crypto"
 
