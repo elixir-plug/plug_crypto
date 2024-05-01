4d441847e8c006ee4e53ac99b62fd2c07e58a0a6
Benjamin Schultzer <benjamin@schultzer.com>
2024-05-01 18:48:37 -0400

    Fix doc for signed_at option

--- lib/plug/crypto.ex
+++ lib/plug/crypto.ex
@@ -168,7 +168,7 @@ defmodule Plug.Crypto do
     * `:key_digest` - option passed to `Plug.Crypto.KeyGenerator`
       when generating the encryption and signing keys. Defaults to `:sha256`
     * `:signed_at` - set the timestamp of the token in seconds.
-      Defaults to `System.system_time(:second)`
+      Defaults to `System.system_time(:millisecond)`
     * `:max_age` - the default maximum age of the token. Defaults to
       `86400` seconds (1 day) and it may be overridden on `verify/4`.
 
@@ -196,7 +196,7 @@ defmodule Plug.Crypto do
     * `:key_digest` - option passed to `Plug.Crypto.KeyGenerator`
       when generating the encryption and signing keys. Defaults to `:sha256`
     * `:signed_at` - set the timestamp of the token in seconds.
-      Defaults to `System.system_time(:second)`
+      Defaults to `System.system_time(:millisecond)`
     * `:max_age` - the default maximum age of the token. Defaults to
       `86400` seconds (1 day) and it may be overridden on `decrypt/4`.
 
